8ab0d301423f5fa1021ec3dc250dfadd10e94148
Darren Victoriano <darren.victoriano@gmail.com>
2020-07-23 21:32:53 -0700

    working on the landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,41 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/navbar/navbar';
+import Helmet from 'react-helmet';
 
 function App() {
   return (
     <div className="App container-fluid px-0">
+      <Helmet bodyAttributes={{ style: 'background-color : #343a40' }} />
       <NavBar />
+      <div className="home-content text-center">
+        <p className="text-light mb-0 p-0 home-content-title">
+          Software Engineer
+        </p>
+        <hr className="title-hr" />
+        <p className="text-muted lead">
+          Specializing in test automation and developing FullStack web application.
+        </p>
+        <div className="media-icons">
+          <a href="https://www.linkedin.com/in/darren-victoriano/" target="_blank" data-toggle="tooltip"
+            data-placement="top" title="Tooltip on top">
+            <i class="fab fa-linkedin fa-2x"></i>
+          </a>
+          <a href="https://github.com/DarrenVictoriano" target="_blank">
+            <i class="fab fa-github fa-2x"></i>
+          </a>
+          {/* <a href="images/VR040.pdf" target="_blank">
+            <i class="fas fa-file-alt fa-2x"></i>
+          </a> */}
+          <a href="mailto:darren.victoriano@gmail.com">
+            <i class="fas fa-envelope fa-2x"></i>
+          </a>
+        </div>
+      </div>
+      {/* <div className="footer text-center text-muted">
+        <hr className="footer-hr" />
+        &copy; Copyright 2020 by Darren Victoriano
+      </div> */}
     </div>
   );
 }
--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -8,27 +8,31 @@ const NavBar = (props) => {
 
     return (
         <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand className="type-width" href="#home">
-                <Typewriter
-                    options={{
-                        strings: ['Darren', 'Victoriano', "へ‿(ツ)‿ㄏ"],
-                        autoStart: true,
-                        loop: true,
-                        pauseFor: 100,
-                        changeDeleteSpeed: 1,
-                        changeDelay: 1
-                    }}
-                />
+            <Navbar.Brand className="brand-name text-light" href="#home">
+                <h1 className="brand-h1">
+                    <Typewriter
+                        options={{
+                            strings: ['Darren', 'Victoriano', "へ‿(ツ)‿ㄏ", "Darren Victoriano"],
+                            autoStart: true,
+                            loop: true,
+                            pauseFor: 100,
+                            changeDeleteSpeed: 1,
+                            changeDelay: 1
+                        }}
+                    />
+                </h1>
             </Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
-                    <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                    <Nav.Link href="#post">Posts</Nav.Link>
+
                 </Nav>
                 <Nav>
-                    <Nav.Link href="#deets"><i class="fas fa-lock"></i></Nav.Link>
+                    <Nav.Link className="px-3" href="#home">Home</Nav.Link>
+                    <Nav.Link className="px-3" href="#portfolio">Portfolio</Nav.Link>
+                    <Nav.Link className="px-3" href="#post">Posts</Nav.Link>
+                    <Nav.Link className="px-3" href="#resume">Resume</Nav.Link>
+                    <Nav.Link className="px-3" href="#deets"><i class="fas fa-lock"></i></Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Navbar>
