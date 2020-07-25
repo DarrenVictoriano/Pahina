ba461f493ef59a2b62e0349b16524cbde7ab66fe
Darren Victoriano <darren.victoriano@gmail.com>
2020-07-25 11:39:19 -0700

    created as seperate component:

---
+++ client/src/components/content/home/home.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './home.css';
+
+const Home = () => {
+
+    return (
+        <div className="home-content text-center">
+            <p className="text-light mb-0 p-0 home-content-title">
+                Software Engineer
+        </p>
+            <hr className="title-hr" />
+            <p className="text-muted lead">
+                Specializing in test automation and developing FullStack web application.
+        </p>
+            <div className="media-icons">
+                <a href="https://www.linkedin.com/in/darren-victoriano/" target="_blank" data-toggle="tooltip"
+                    data-placement="top" title="Tooltip on top">
+                    <i class="fab fa-linkedin fa-2x"></i>
+                </a>
+                <a href="https://github.com/DarrenVictoriano" target="_blank">
+                    <i class="fab fa-github fa-2x"></i>
+                </a>
+                {/* <a href="images/VR040.pdf" target="_blank">
+            <i class="fas fa-file-alt fa-2x"></i>
+          </a> */}
+                <a href="mailto:darren.victoriano@gmail.com">
+                    <i class="fas fa-envelope fa-2x"></i>
+                </a>
+            </div>
+        </div>
+    );
+}
+
+export default Home;
