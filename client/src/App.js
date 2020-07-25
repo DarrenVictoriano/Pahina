f8c476b778ad0259f37bb2a10b3f7a79d675d280
Darren Victoriano <darren.victoriano@gmail.com>
2020-07-25 11:56:50 -0700

    added Context API and React-Reponsive

--- client/src/App.js
+++ client/src/App.js
@@ -3,16 +3,20 @@ import './App.css';
 import NavBar from './components/navbar/navbar';
 import Helmet from 'react-helmet';
 import Home from './components/content/home/home';
+import { PostProvider } from './providers/postContext';
 
 function App() {
 
   return (
-    <div className="App container-fluid px-0">
-      <Helmet bodyAttributes={{ style: 'background-color : #343a40' }} />
-      <NavBar />
-      <Home />
+    <PostProvider>
+      <div className="App container-fluid px-0">
 
-    </div>
+        <Helmet bodyAttributes={{ style: 'background-color : #343a40' }} />
+        <NavBar />
+        <Home />
+
+      </div>
+    </PostProvider>
   );
 }
 
