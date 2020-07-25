ebe6880bc34d2daa5442e01916d243b8be3db558
Darren Victoriano <darren.victoriano@gmail.com>
2020-07-25 11:57:02 -0700

    placeholder

---
+++ client/src/providers/postContext.js
@@ -0,0 +1,25 @@
+import React, { useState, createContext } from 'react';
+import { useMediaQuery } from 'react-responsive';
+
+export const PostContext = createContext();
+
+export const PostProvider = (props) => {
+    // this will return true if viewport is less than 500px
+    const isMobile = useMediaQuery({ query: '(max-width: 500px)' });
+
+    // this will hold all my blog post
+    const [postData, setPostData] = useState({
+        "title": null,
+        "date": null,
+        "body": null
+    });
+
+    return (
+        <PostContext.Provider value={{
+            "mobileCheckState": isMobile,
+            "postDataState": [postData, setPostData]
+        }}>
+            {props.children}
+        </PostContext.Provider>
+    );
+}
