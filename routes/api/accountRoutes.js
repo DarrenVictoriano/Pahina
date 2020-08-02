5d4dd9b8a1f6bce7525f8b65c6d9b608ecf3980c
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-02 15:37:04 -0700

    fixed route description

--- routes/api/accountRoutes.js
+++ routes/api/accountRoutes.js
@@ -4,24 +4,24 @@ const accountController = require('../../controllers/accountController');
 
 
 router.route("/")
-    // @route       api/user/
+    // @route       api/v1/user/
     // @desc-GET    Read All User, DEV use only
     .get(makePrivate, accountController.readAllDevOnly)
-    // @route       api/user/
+    // @route       api/v1/user/
     // @desc-POST   Register new user
     .post(accountController.registerAccount);
 
-// @route       api/user/auth
+// @route       api/v1/user/auth
 // @desc-POST   Login user
 router.route("/auth")
     .post(accountController.authenticateUser);
 
 
 router.route("/:id")
-    // @route       api/user/_id
+    // @route       api/v1/user/_id
     // @desc-GET    Get account info
     .get(makePrivate, accountController.getAccountInfo)
-    // @route       api/user/_id
+    // @route       api/v1/user/_id
     // @desc-DEL    Delete account
     .delete(makePrivate, accountController.deleteAccount);
 
