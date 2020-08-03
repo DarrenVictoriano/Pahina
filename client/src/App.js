bc5ee40ad4a9be6c6dd7e601d591c0ebeb7e8b34
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-02 21:20:08 -0700

    refactor Route

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import Home from './components/content/home/home';
 import { PostProvider } from './providers/postContext';
 import Portfolio from './components/content/porfolio/portfolio';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import PostPage from './components/content/posts/postPage';
+import PostBody from './components/content/posts/postBody';
 
 function App() {
 
@@ -18,13 +20,10 @@ function App() {
             <Helmet bodyAttributes={{ style: 'background-color : #0a192f' }} />
             <NavBar />
 
-            <Route exact path="/">
-              <Home />
-            </Route>
-
-            <Route path="/portfolio">
-              <Portfolio />
-            </Route>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route exact path="/posts" component={PostPage} />
+            <Route exact path="/post/:id" component={PostBody} />
 
           </div>
         </PostProvider>
