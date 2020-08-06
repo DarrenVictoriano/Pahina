4c3b029db3379813321406f97d4b21e219ae223f
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-05 21:34:12 -0700

    removed dummy data, replaced by actual data from the DB using useEffect hook

--- client/src/components/content/posts/postPage.js
+++ client/src/components/content/posts/postPage.js
@@ -1,29 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import { PostContext } from '../../../providers/postContext';
 import './postStyles.css';
 import PostThumbnail from './postThumbnail';
+import axios from 'axios';
 
 const PostPage = () => {
 
     const { mobileCheckState } = useContext(PostContext);
     const isMobile = mobileCheckState;
+    const [post, setPost] = useState([]);
 
-    const dummyData = [
-        {
-            id: "12323",
-            title: "Sample Title",
-            overview: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
-            body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
-        }
-    ]
+    useEffect(() => {
+        // get all post
+        axios.get("/api/v1/post/")
+            .then(allPost => {
+                setPost(allPost.data)
+            })
+            .catch(err => {
+                console.log(err);
+            })
+
+    }, []);
 
     return (
         <div className={"container text-slate " + (isMobile ? "pt-8" : "pt-10")}>
-            <PostThumbnail id={dummyData[0].id} title={dummyData[0].title} overview={dummyData[0].overview} />
-
-            <PostThumbnail id={dummyData[0].id} title={dummyData[0].title} overview={dummyData[0].overview} />
-
-            <PostThumbnail id={dummyData[0].id} title={dummyData[0].title} overview={dummyData[0].overview} />
+            {
+                post.map((item) => (
+                    <PostThumbnail key={item._id} id={item._id} title={item.title} date={item.date_created} overview={item.overview} />
+                ))
+            }
         </div>
     );
 }
