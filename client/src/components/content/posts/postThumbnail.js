5abb27465631f0f407776228b2679448aa182783
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-05 21:35:54 -0700

    added edit and delete button, need to update this so that buttons only show on admin account

--- client/src/components/content/posts/postThumbnail.js
+++ client/src/components/content/posts/postThumbnail.js
@@ -1,19 +1,32 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { PostContext } from '../../../providers/postContext';
+import Moment from 'react-moment';
 import Button from 'react-bootstrap/Button';
 
-const PostThumbnail = ({ id, title, overview }) => {
+const PostThumbnail = ({ id, title, overview, date }) => {
 
     const { mobileCheckState } = useContext(PostContext);
     const isMobile = mobileCheckState;
 
     return (
         <div className={"bg-navy-light rounded container " + (isMobile ? "p-3 mb-3" : "p-5 mb-5")}>
-
-            <h1 className={"text-slate-light "}>{title}</h1>
-            <p className={"text-slate-light"}>{overview}</p>
-            <Link exact to={"/post/" + id} className={"read-article text-slate-lighter "}>Read Article</Link>
+            <div className="row">
+                <div className="col-lg-11">
+                    <h1 className={"text-slate-lighter mb-0"}>{title}</h1>
+                    <p className={"text-slate-light"}><Moment format="MMMM DD, YYYY" date={date} /></p>
+                    <p className={"text-slate-light"}>{overview}</p>
+                    <Link exact to={"/blog/" + id} className={"read-article text-slate-lighter "}>Read Article</Link>
+                </div>
+                <div className={"col-lg-1 " + (isMobile ? "mt-2" : "")}>
+                    <Button className={"btn-sm btn-size " + (isMobile ? "mr-3" : "mb-3")}>
+                        Edit
+                    </Button>
+                    <Button className="btn-danger btn-sm btn-size ">
+                        Delete
+                    </Button>
+                </div>
+            </div>
         </div>
     );
 }
