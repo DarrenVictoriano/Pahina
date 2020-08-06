38a28112377e6ec1a0756cd3deafe41d7fcfbb64
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-05 21:49:39 -0700

    added states but might removed later

--- client/src/providers/postContext.js
+++ client/src/providers/postContext.js
@@ -7,17 +7,34 @@ export const PostProvider = (props) => {
     // this will return true if viewport is less than 500px
     const isMobile = useMediaQuery({ query: '(max-width: 991px)' });
 
+    const [headerConfig, setHeaderConfig] = useState({
+        "headers": {
+            "x-header-token": localStorage.getItem("token")
+        }
+    });
+
+    const [userID, setUserID] = useState("");
+
     // this will hold all my blog post
-    const [postData, setPostData] = useState({
-        "title": null,
-        "date": null,
-        "body": null
+    const [accountInfo, setAccountInfo] = useState({
+        "token": "",
+        "_id": "",
+        "username": "",
+        "posts": [{
+            "_id": "",
+            "title": "",
+            "overview": "",
+            "body": "",
+            "date_created": ""
+        }]
     });
 
     return (
         <PostContext.Provider value={{
             "mobileCheckState": isMobile,
-            "postDataState": [postData, setPostData]
+            "accountInfoState": [accountInfo, setAccountInfo],
+            "headerConfigState": [headerConfig, setHeaderConfig],
+            "userIDState": [userID, setUserID]
         }}>
             {props.children}
         </PostContext.Provider>
