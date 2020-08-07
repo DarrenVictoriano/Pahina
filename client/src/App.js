ce91d0df9d92d6667b13b801af3ee7931f66c090
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-06 21:07:17 -0700

    added cookies and refactor switch to fix bug

--- client/src/App.js
+++ client/src/App.js
@@ -10,30 +10,35 @@ import PostPage from './components/content/posts/postPage';
 import PostBody from './components/content/posts/postBody';
 import PostForm from './components/content/posts/postForm';
 import Login from './components/auth/login';
+import { useCookies } from 'react-cookie';
+import PageNotFound from './components/notfound/pageNotFound';
 
 function App() {
 
+  const [cookies] = useCookies(['token']);
+
   return (
-    <Router>
-      <Switch>
-        <PostProvider>
-          <div className="App container-fluid px-0">
+    <PostProvider>
+      <Router>
+        <div className="App container-fluid px-0">
 
-            <Helmet bodyAttributes={{ style: 'background-color : #0a192f' }} />
-            <NavBar />
+          <Helmet bodyAttributes={{ style: 'background-color : #0a192f' }} />
+          <NavBar />
 
+          <Switch> {/* switch should only contain routes and no other elements, otherwise it wont work correctly */}
             <Route exact path="/" component={Home} />
             <Route exact path="/portfolio" component={Portfolio} />
-            <Route exact path="/blog" component={PostPage} />
-            <Route exact path="/blog/:id" component={PostBody} />
-            <Route exact path="/postform" component={PostForm} />
+            <Route exact path="/postform" component={cookies.token ? PostForm : PageNotFound} />
             <Route exact path="/deets" component={Login} />
+            <Route exact path="/blog" component={PostPage} />
+            <Route path="/blog/:id" component={PostBody} />
 
-          </div>
-        </PostProvider>
-      </Switch>
-    </Router>
+            <Route component={PageNotFound} />
+          </Switch>
 
+        </div>
+      </Router>
+    </PostProvider>
   );
 }
 
