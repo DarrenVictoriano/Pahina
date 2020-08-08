f9607dc940ec017ce981b771809fafbe081c2003
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-07 18:52:02 -0700

    TODO: Markdown does not render code higlihting yet

--- client/src/components/content/posts/postBody.js
+++ client/src/components/content/posts/postBody.js
@@ -5,14 +5,33 @@ import { useParams } from 'react-router-dom';
 import ReactMarkdown from 'react-markdown';
 import Moment from 'react-moment';
 import axios from 'axios';
+// import MarkdownIt from 'markdown-it';
+// import hljs from 'highlight.js';
 
 const PostBody = (props) => {
 
+    // get the ID from the URL
     const { id } = useParams();
+
     const { mobileCheckState } = useContext(PostContext);
     const [post, setPost] = useState({});
 
     const isMobile = mobileCheckState;
+
+    // TODO: fix mardown with syntax highlighting
+    // let md = MarkdownIt()({
+    //     highlight: function (str, lang) {
+    //         if (lang && hljs.getLanguage(lang)) {
+    //             try {
+    //                 return '<pre class="hljs"><code>' +
+    //                     hljs.highlight(lang, str, true).value +
+    //                     '</code></pre>';
+    //             } catch (__) { }
+    //         }
+
+    //         return '<pre class="hljs"><code>' + md.utils.escapeHtml(str) + '</code></pre>';
+    //     }
+    // });
 
     useEffect(() => {
         axios.get("/api/v1/post/" + id)
@@ -22,7 +41,7 @@ const PostBody = (props) => {
             .catch(err => {
                 console.log(err);
             });
-    }, [])
+    }, [id])
 
 
     return (
@@ -35,6 +54,7 @@ const PostBody = (props) => {
                 <ReactMarkdown
                     source={post.body}
                 />
+                {/* {md.render(post.body)} */}
 
             </div>
         </div>
