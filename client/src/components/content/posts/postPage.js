13baa99e09868905fba7e74a7535108b02443ca9
Darren Victoriano <darren.victoriano@gmail.com>
2020-08-07 18:53:41 -0700

    changed post state into allPostState which is an API Context (global state)

--- client/src/components/content/posts/postPage.js
+++ client/src/components/content/posts/postPage.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { PostContext } from '../../../providers/postContext';
 import './postStyles.css';
 import PostThumbnail from './postThumbnail';
@@ -6,15 +6,15 @@ import axios from 'axios';
 
 const PostPage = () => {
 
-    const { mobileCheckState } = useContext(PostContext);
+    const { mobileCheckState, allPostState } = useContext(PostContext);
     const isMobile = mobileCheckState;
-    const [post, setPost] = useState([]);
+    const [allPost, setAllPost] = allPostState;
 
     useEffect(() => {
         // get all post
         axios.get("/api/v1/post/")
             .then(allPost => {
-                setPost(allPost.data)
+                setAllPost(allPost.data)
             })
             .catch(err => {
                 console.log(err);
@@ -25,7 +25,7 @@ const PostPage = () => {
     return (
         <div className={"container text-slate " + (isMobile ? "pt-8" : "pt-10")}>
             {
-                post.map((item) => (
+                allPost.map((item) => (
                     <PostThumbnail key={item._id} id={item._id} title={item.title} date={item.date_created} overview={item.overview} />
                 ))
             }
